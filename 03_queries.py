751c2a159482d81e539c409b51033c08a2d454a0
Lais Melo <smelc.lais@gmail.com>
2026-04-07 23:18:01 -0300

    feat: adiciona bloco de ranking de artistas

--- 03_queries.py
+++ 03_queries.py
@@ -8,9 +8,8 @@ data_path = os.path.join(file_path, 'streaming_music.db')
 
 conn = sqlite3.connect(data_path)
 
-# 2. criar query
-# utilizar JOIN para ter Usuário e Música juntos, e CASE para mostrar se o usuário ouviu a música até o fim ou pulou.
-query = """
+# 2. criar query de relatório geral
+query_relatorio = """
 SELECT 
     Users.nome AS Usuario,
     Musicas.titulo AS Musica,
@@ -25,10 +24,30 @@ JOIN Users ON Streams.id_user = Users.id_user
 JOIN Musicas ON Streams.id_musica = Musicas.id_musica;
 """
 
-# 3. executar e exibir resultado
-df_relatorio = pd.read_sql_query(query, conn)
+# executar e exibir resultado do relatório geral
+df_relatorio = pd.read_sql_query(query_relatorio, conn)
 
 print("\n--- RELATÓRIO DE STREAMING ---")
 print(df_relatorio.to_string(index=False))
+
+# 4. criar query de ranking
+query_ranking = """
+SELECT 
+    Musicas.artista AS Artista,
+    COUNT(Streams.id_stream) AS Plays_Totais,
+    SUM(Streams.completada) AS Plays_Ate_O_Fim
+FROM Streams
+JOIN Musicas ON Streams.id_musica = Musicas.id_musica
+GROUP BY Musicas.artista
+ORDER BY Plays_Totais DESC;
+"""
+
+# executar e exibir resultado do ranking
+df_ranking = pd.read_sql_query(query_ranking, conn)
+
+print("")
+print("")
+print("\n--- RANKING DE ARTISTAS ---")
+print(df_ranking.to_string(index=False))
 
 conn.close()
